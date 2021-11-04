b4ec0d11f2cdb2679034074863e91d7a911fa00d
rizkys15 <kikuyyy15@gmail.com>
2021-11-04 12:00:53 +0700

    menambahkan lingkaran.py

---
+++ lingkaran.py
@@ -0,0 +1,8 @@
+import math
+r = float(input("masukan jari-jari:"))
+
+luas = math.pi*(r*r)
+keliling = 2*math.pi*r
+
+print("luas lingkaran \t= ",format(luas))
+print("keliling lingkaran \t= ",format(keliling))
